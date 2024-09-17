6bb233b77c689245a29cb4399c93f9365746cdb2
ChrisKod007 <cdbm001@gmail.com>
2024-09-16 21:30:04 -0500

    Update main.py
    Este bot tiene muchas mas funciones nuevas

--- main.py
+++ main.py
@@ -22,4 +22,29 @@ async def heh(ctx, count_heh = 5):
 async def joined(ctx, member: discord.Member):
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
 bot.run("EL TOKEN SECRETO VA AQUÍ")
